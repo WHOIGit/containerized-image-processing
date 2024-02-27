d7e9f4ba42f19acafdb75bf4ec891f78c8440057
Joe Futrelle <jfutrelle@whoi.edu>
2024-02-27 09:03:31 -0500

    run dithering in a separate process so as not to block event loop

--- app/api.py
+++ app/api.py
@@ -1,3 +1,5 @@
+import asyncio
+from concurrent.futures import ProcessPoolExecutor
 from fastapi import FastAPI, File, UploadFile, Form
 from fastapi.responses import StreamingResponse
 from io import BytesIO
@@ -8,13 +10,17 @@ from floyd_steinberg import floyd_steinberg_dithering
 
 app = FastAPI()
 
+executor = ProcessPoolExecutor(max_workers=5)
+
 @app.post("/dither/")
 async def dither_image(file: UploadFile = File(...)):
     image_bytes = await file.read()  # Read the image file as bytes
     input_image = Image.open(BytesIO(image_bytes))  # Open the image using PIL
 
-    # Process the image
-    processed_image = floyd_steinberg_dithering(input_image)
+    # Process the image w/o blocking the event loop
+    loop = asyncio.get_event_loop()
+    future = executor.submit(floyd_steinberg_dithering, input_image)
+    processed_image = await loop.run_in_executor(None, future.result)
     
     output = BytesIO()
     processed_image.save(output, format="PNG")
